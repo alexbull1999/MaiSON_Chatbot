8c0a4c13b239036b376a6e58015acdab4a805323
Alex Bull <alexbull1999@gmail.com>
2025-03-05 17:20:16 +0000

    Tweak to database restarting logic

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,4 @@ from .config import settings
 from .database import Base, engine, get_db
 from .api.routes import router
 
-# Create database tables
-Base.metadata.create_all(bind=engine)
-
 __all__ = ["settings", "Base", "engine", "get_db", "router"]
